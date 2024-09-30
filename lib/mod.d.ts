53cde17c87a528a5ff2f25723339fcf6ce7ef737
yjgaia <yjdev88@gmail.com>
2024-09-30 16:35:23 +0900

    Add Tag, DomSelector, and ElFunction modules

---
+++ lib/mod.d.ts
@@ -0,0 +1,4 @@
+export type { default as Tag } from "./Tag.ts";
+export type { default as DomSelector } from "./DomSelector.ts";
+export type { default as ElFunction } from "./ElFunction.ts";
+//# sourceMappingURL=mod.d.ts.map
