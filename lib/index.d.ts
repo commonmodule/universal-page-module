9520913ca04d8b0794470d83d142f80bf8caa6f8
yjgaia <yjdev88@gmail.com>
2024-10-03 15:35:17 +0900

    Refactor ElFunction import and export statements

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,3 @@
 export { default as DomSelector } from "./DomSelector.js";
-export { default as ElFunction } from "./ElFunction.js";
 export { default as Tag } from "./Tag.js";
 //# sourceMappingURL=index.d.ts.map
--- lib/mod.d.ts
+++ lib/mod.d.ts
@@ -1,4 +1,3 @@
 export type { default as Tag } from "./Tag.ts";
 export type { default as DomSelector } from "./DomSelector.ts";
-export type { default as ElFunction } from "./ElFunction.ts";
 //# sourceMappingURL=mod.d.ts.map
