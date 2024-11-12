94ea4636cbfd0522db1b88fc70e622260fe483cc
yjgaia <yjdev88@gmail.com>
2024-11-12 15:24:00 +0900

    Update import paths to reference .ts files in mod.ts

--- deno/mod.ts
+++ deno/mod.ts
@@ -1,7 +1,7 @@
-export type { default as DomSelector } from "../lib/DomSelector.d.ts";
+export type { default as DomSelector } from "../src/DomSelector.ts";
 export { default as el } from "../lib/el.js";
-export type { DomChild } from "../lib/el.d.ts";
-export type { default as ElementOrSelector } from "../lib/ElementOrSelector.d.ts";
-export type { default as ElementProperties } from "../lib/ElementProperties.d.ts";
+export type { DomChild } from "../src/el.ts";
+export type { default as ElementOrSelector } from "../src/ElementOrSelector.ts";
+export type { default as ElementProperties } from "../src/ElementProperties.ts";
 export { default as html } from "../lib/html.js";
-export type { default as InferElementType } from "../lib/InferElementType.d.ts";
+export type { default as InferElementType } from "../src/InferElementType.ts";
